169ee90c5720bd10c7980233e7e9e5130b067edb
bentobox1232 <histanley7@gmail.com>
2024-01-23 22:44:39 -0700

    Queen done

---
+++ shared/src/main/java/chess/Pieces/Queen.java
@@ -0,0 +1,91 @@
+package chess.Pieces;
+
+import chess.*;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class Queen implements ChessPieceInterface {
+
+    private ChessGame.TeamColor pieceColor;
+
+    public Queen(ChessGame.TeamColor pieceColor) {
+        this.pieceColor = pieceColor;
+    }
+
+    @Override
+    public ChessGame.TeamColor getTeamColor() {
+        return pieceColor;
+    }
+
+    @Override
+    public ChessPiece.PieceType getPieceType() {
+        return ChessPiece.PieceType.QUEEN;
+    }
+
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> validMoves = new HashSet<>();
+
+        addDiagonalMoves(board, myPosition, validMoves, 1, 1);
+
+        addDiagonalMoves(board, myPosition, validMoves, 1, -1);
+
+        addDiagonalMoves(board, myPosition, validMoves, -1, 1);
+
+        addDiagonalMoves(board, myPosition, validMoves, -1, -1);
+
+        addSideMoves(board, myPosition, validMoves, 0, 1);
+
+        addSideMoves(board, myPosition, validMoves, 0, -1);
+
+        addSideMoves(board, myPosition, validMoves, 1, 0);
+
+        addSideMoves(board, myPosition, validMoves, -1, 0);
+
+        return validMoves;
+    }
+
+    private void addDiagonalMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> validMoves, int rowDirection, int colDirection) {
+        int row = myPosition.getRow() + rowDirection;
+        int col = myPosition.getColumn() + colDirection;
+
+        while (board.isValidPosition(row, col)) {
+            ChessPosition endPosition = new ChessPosition(row, col);
+            ChessPiece targetPiece = board.getPiece(endPosition);
+
+            if (targetPiece == null || targetPiece.getTeamColor() != getTeamColor()) {
+                validMoves.add(new ChessMove(myPosition, endPosition, null));
+            }
+
+            if (targetPiece != null) {
+                break;
+            }
+
+            row += rowDirection;
+            col += colDirection;
+        }
+    }
+
+    private void addSideMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> validMoves, int rowDirection, int colDirection) {
+        int row = myPosition.getRow() + rowDirection;
+        int col = myPosition.getColumn() + colDirection;
+
+        while (board.isValidPosition(row, col)) {
+            ChessPosition endPosition = new ChessPosition(row, col);
+            ChessPiece targetPiece = board.getPiece(endPosition);
+
+            if (targetPiece == null || targetPiece.getTeamColor() != getTeamColor()) {
+                validMoves.add(new ChessMove(myPosition, endPosition, null));
+            }
+
+            if (targetPiece != null) {
+                break;
+            }
+
+            row += rowDirection;
+            col += colDirection;
+        }
+    }
+}
+
