ccbef88ad20ac651293d524ab29a751182728ca7
bentobox1232 <histanley7@gmail.com>
2024-03-06 00:23:18 -0700

    all tests are passed in

---
+++ shared/src/main/java/request/LogoutRequest.java
@@ -0,0 +1,13 @@
+package request;
+
+public class LogoutRequest {
+    private String authToken;
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+}
